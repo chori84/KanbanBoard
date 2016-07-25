a8be5ccc783f52e6ccdaf9ecb49c69c1134c4045
Chori <slivers721@gmail.com>
2016-07-25 22:26:29 +0900

    칸반 보드  - 속성 유효성 검사 및 App.js에 테스트용 데이터 추가

--- source/App.js
+++ source/App.js
@@ -3,38 +3,46 @@ import { render } from 'react-dom';
 import KanbanBoard from './KanbanBoard';
 
 let cardsList = [
-    {
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the **whole** book",
+    color: "#BD8D31",
+    status: 'in-progress',
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book. The complate source can be found at [github](http://github.com//pro-react)",
+    color: "#3A7E28",
+    status: 'todo',
+    tasks: [
+      {
         id: 1,
-        title: "Read the Book",
-        description: "I should read the **whole** book",
-        color: "#BD8D31",
-        status: 'in-progress',
-        tasks: []
-    },
-    {
+        name: "ContactList Example",
+        done: true
+      },
+      {
         id: 2,
-        title: "Write some code",
-        description: "Code along with the samples in the book. The complate source can be found at [github](http://github.com//pro-react)",
-        color: "#3A7E28",
-        status: 'todo',
-        tasks: [
-            {
-                id: 1,
-                name: "ContactList Example",
-                done: true
-            },
-            {
-                id: 2,
-                name: "Kanban Example",
-                done: false
-            },
-            {
-                id: 3,
-                name: "My own experiments",
-                done: false
-            },
-        ]
-    }
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: "This is Card title propTypes test. fsakjfh;salkfjsa;lfkjs;lfkjas;lfkjdas;lfkjas;lfkjslfkjaslkasflkdjaflksdjf;lakjfasl;kdfjasldfkjdsalfhjdkxjvhzkj",
+    description: "Test",
+    color: "#FFFFFF",
+    status: 'done',
+    tasks: []
+  }
 ];
 
 render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    entry: [
-        './source/App.js'
-    ],
-    output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }]
-    },
-    devServer: {
-        contentBase: './public',
-        colors: true,
-        port: 8088,
-        historyApiFallback: true,
-        inline: true
-    }
+  entry: [
+    './source/App.js'
+  ],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+  devServer: {
+    contentBase: './public',
+    colors: true,
+    port: 8088,
+    historyApiFallback: true,
+    inline: true
+  }
 }
