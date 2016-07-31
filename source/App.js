f3bc85915efbcf80b72e7d02fc440f5141fa32cd
Chori <slivers721@gmail.com>
2016-07-31 21:16:58 +0900

    칸반 보드  - 계층의 맨 위에서 데이터 가져오기/지속성의 역할을 수행하기 위해 컨테이너 추가  - 하드 코딩 된 데이터를 서버에서 가져오도록 변경  - 테스트 콜백을 속성과 연결하고, 서버에 변경 된 데이터 반영    - 테스크 추가    - 테스크 삭제    - 테스크 체크박스

--- source/App.js
+++ source/App.js
@@ -1,48 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import KanbanBoard from './KanbanBoard';
+import KanbanBoardContainer from './KanbanBoardContainer';
 
-let cardsList = [
-  {
-    id: 1,
-    title: "Read the Book",
-    description: "I should read the **whole** book",
-    color: "#BD8D31",
-    status: 'in-progress',
-    tasks: []
-  },
-  {
-    id: 2,
-    title: "Write some code",
-    description: "Code along with the samples in the book. The complate source can be found at [github](http://github.com//pro-react)",
-    color: "#3A7E28",
-    status: 'todo',
-    tasks: [
-      {
-        id: 1,
-        name: "ContactList Example",
-        done: true
-      },
-      {
-        id: 2,
-        name: "Kanban Example",
-        done: false
-      },
-      {
-        id: 3,
-        name: "My own experiments",
-        done: false
-      },
-    ]
-  },
-  {
-    id: 3,
-    title: "This is Card title propTypes test. fsakjfh;salkfjsa;lfkjs;lfkjas;lfkjdas;lfkjas;lfkjslfkjaslkasflkdjaflksdjf;lakjfasl;kdfjasldfkjdsalfhjdkxjvhzkj",
-    description: "Test",
-    color: "#FFFFFF",
-    status: 'done',
-    tasks: []
-  }
-];
-
-render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+render(<KanbanBoardContainer />, document.getElementById('root'));
--- source/List.js
+++ source/List.js
@@ -3,13 +3,14 @@ import Card from './Card';
 
 class List extends Component {
   render() {
-    var cards = this.props.cards.map((card) => {
-      return <Card id={card.id}
+    let cards = this.props.cards.map((card) => {
+      return <Card key={card.id}
+                   taskCallbacks={this.props.taskCallbacks}
+                   id={card.id}
                    title={card.title}
                    description={card.description}
                    color={card.color}
-                   tasks={card.tasks}
-                   key={card.id} />
+                   tasks={card.tasks} />
     });
 
     return (
@@ -23,7 +24,8 @@ class List extends Component {
 
 List.propTypes = {
   title: PropTypes.string.isRequired,
-  cards: PropTypes.arrayOf(PropTypes.object)
+  cards: PropTypes.arrayOf(PropTypes.object),
+  taskCallbacks: PropTypes.object
 };
 
 export default List;
